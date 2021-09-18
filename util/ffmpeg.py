52ad7373a23fec65156562fb9444ee5516d07f31
orenodinner <orenodinner@gmail.com>
2021-09-18 23:35:03 +0900

    -hwaccel cuda

--- util/ffmpeg.py
+++ util/ffmpeg.py
@@ -1,33 +1,43 @@
-import os,json
+import os
+import json
 
 # ffmpeg 3.4.6
 
-def video2image(videopath,imagepath,fps=0,start_time=0,last_time=0):
+
+def video2image(videopath, imagepath, fps=0, start_time=0, last_time=0):
     if start_time == 0:
         if fps == 0:
-            os.system('ffmpeg -i "'+videopath+'" -f image2 '+'-q:v -0 '+imagepath)
+            os.system('ffmpeg -hwaccel cuda -i "'+videopath +
+                      '" -f image2 '+'-q:v -0 '+imagepath)
         else:
-            os.system('ffmpeg -i "'+videopath+'" -r '+str(fps)+' -f image2 '+'-q:v -0 '+imagepath)
+            os.system('ffmpeg -hwaccel cuda  -i "'+videopath+'" -r '+str(fps) +
+                      ' -f image2 '+'-q:v -0 '+imagepath)
     else:
         if fps == 0:
-            os.system('ffmpeg -ss '+start_time+' -t '+last_time+' -i "'+videopath+'" -f image2 '+'-q:v -0 '+imagepath)
+            os.system('ffmpeg -hwaccel cuda  -ss '+start_time+' -t '+last_time +
+                      ' -i "'+videopath+'" -f image2 '+'-q:v -0 '+imagepath)
         else:
-            os.system('ffmpeg -ss '+start_time+' -t '+last_time+' -i "'+videopath+'" -r '+str(fps)+' -f image2 '+'-q:v -0 '+imagepath)
+            os.system('ffmpeg -hwaccel cuda  -ss '+start_time+' -t '+last_time+' -i "' +
+                      videopath+'" -r '+str(fps)+' -f image2 '+'-q:v -0 '+imagepath)
 
 
-def video2voice(videopath,voicepath):
+def video2voice(videopath, voicepath):
     os.system('ffmpeg -i "'+videopath+'" -f mp3 '+voicepath)
 
-def image2video(fps,imagepath,voicepath,videopath):
-    os.system('ffmpeg -y -r '+str(fps)+' -i '+imagepath+' -vcodec hevc_nvenc '+'./tmp/video_tmp.mp4')
+
+def image2video(fps, imagepath, voicepath, videopath):
+    os.system('ffmpeg -y -r '+str(fps)+' -i '+imagepath +
+              ' -vcodec hevc_nvenc '+'./tmp/video_tmp.mp4')
     #os.system('ffmpeg -f image2 -i '+imagepath+' -vcodec libx264 -r '+str(fps)+' ./tmp/video_tmp.mp4')
-    os.system('ffmpeg -i ./tmp/video_tmp.mp4 -i "'+voicepath+'" -vcodec copy -acodec copy '+videopath)
+    os.system('ffmpeg -i ./tmp/video_tmp.mp4 -i "'+voicepath +
+              '" -vcodec copy -acodec copy '+videopath)
+
 
 def get_video_infos(videopath):
-    cmd_str =  'ffprobe -v quiet -print_format json -show_format -show_streams -i "' + videopath + '"'  
+    cmd_str = 'ffprobe -v quiet -print_format json -show_format -show_streams -i "' + videopath + '"'
     #out_string = os.popen(cmd_str).read()
-    #For chinese path in Windows
-    #https://blog.csdn.net/weixin_43903378/article/details/91979025
+    # For chinese path in Windows
+    # https://blog.csdn.net/weixin_43903378/article/details/91979025
     stream = os.popen(cmd_str)._stream
     out_string = stream.buffer.read().decode(encoding='utf-8')
 
@@ -43,21 +53,27 @@ def get_video_infos(videopath):
         width = int(infos['streams'][1]['width'])
         height = int(infos['streams'][1]['height'])
 
-    return fps,endtime,height,width
+    return fps, endtime, height, width
 
-def cut_video(in_path,start_time,last_time,out_path,vcodec='h265'):
+
+def cut_video(in_path, start_time, last_time, out_path, vcodec='h265'):
     if vcodec == 'copy':
-        os.system('ffmpeg -ss '+start_time+' -t '+last_time+' -i "'+in_path+'" -vcodec copy -acodec copy '+out_path)
-    elif vcodec == 'h264':    
-        os.system('ffmpeg -ss '+start_time+' -t '+last_time+' -i "'+in_path+'" -vcodec libx264 -b 12M '+out_path)
+        os.system('ffmpeg -ss '+start_time+' -t '+last_time+' -i "' +
+                  in_path+'" -vcodec copy -acodec copy '+out_path)
+    elif vcodec == 'h264':
+        os.system('ffmpeg -ss '+start_time+' -t '+last_time +
+                  ' -i "'+in_path+'" -vcodec libx264 -b 12M '+out_path)
     elif vcodec == 'h265':
-        os.system('ffmpeg -ss '+start_time+' -t '+last_time+' -i "'+in_path+'" -vcodec libx265 -b 12M '+out_path)
+        os.system('ffmpeg -ss '+start_time+' -t '+last_time +
+                  ' -i "'+in_path+'" -vcodec libx265 -b 12M '+out_path)
 
-def continuous_screenshot(videopath,savedir,fps):
+
+def continuous_screenshot(videopath, savedir, fps):
     '''
     videopath: input video path
     savedir:   images will save here
     fps:       save how many images per second
     '''
     videoname = os.path.splitext(os.path.basename(videopath))[0]
-    os.system('ffmpeg -i "'+videopath+'" -vf fps='+str(fps)+' -q:v -0 '+savedir+'/'+videoname+'_%06d.jpg')
+    os.system('ffmpeg -i "'+videopath+'" -vf fps='+str(fps) +
+              ' -q:v -0 '+savedir+'/'+videoname+'_%06d.jpg')
